4802793d26e34914edbe913e4db0a20ae2a4c5e7
Jeff Mullins <jtmullins64@gmail.com>
2018-02-18 17:19:55 -0600

    edit

--- hello.py
+++ hello.py
@@ -3,3 +3,4 @@ for animal in animals:
     print(animal.title() + " is a good pet")
 message = "Except " + animals[0].title() + " is way cuter and " + animals[1].title() + " is the fattest cat ever."
 print(message)
+print("Just kidding")
