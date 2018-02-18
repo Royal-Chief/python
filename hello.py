2bc9bc29f9acca2054fbeec0dff391ffe6006a19
Jeff Mullins <jtmullins64@gmail.com>
2018-02-18 17:14:12 -0600

    edit

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 animals = ['kona','boj']
 for animal in animals:
     print(animal.title() + " is a good pet")
-message = "Except " + animals[0].title() + " is way cuter and " + animals[1].title() + " is fat."
+message = "Except " + animals[0].title() + " is way cuter and " + animals[1].title() + " is the fattest."
 print(message)
