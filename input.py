d59fbff6ab22d178ae7e2a43a629765c530c0cf9
Jeff Mullins <jtmullins64@gmail.com>
2018-02-18 21:30:07 -0600

    new files

---
+++ input.py
@@ -0,0 +1,8 @@
+user = input("Enter name: ")
+age = input("Enter age: ")
+age = int(age)
+old_age = str((2018-age)+100)
+print("Your name is " + user + " and you are " + str(age) + " years old.")
+print("You will be 100 in the year " + old_age + ".")
+
+
