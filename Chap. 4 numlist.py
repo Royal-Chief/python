75b80bec10c80a10656e1f63730875605694b202
Jeff Mullins <jtmullins64@gmail.com>
2018-02-19 19:36:03 -0600

    chap 4

---
+++ Chap. 4 numlist.py
@@ -0,0 +1,19 @@
+#loop number
+for value in range(1,6):
+    print(value)
+#list number
+numbers = list(range(1,6))
+print(numbers)
+#even numbers
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+#square
+squares= []
+for value in range(1,11):
+    squares.append(value**2)
+print(squares)
+#digits
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
---
+++ numlistPractice.py
@@ -0,0 +1,11 @@
+for num in range (1,20):
+    print(num)
+mil = list(range (1,10000000))
+print(min(mil))
+print(max(mil))
+
+for odd in range(1,20,2):
+    print(odd)
+
+mult = [value**3 for value in range (3,30)]
+print(mult)
