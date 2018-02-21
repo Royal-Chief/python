50b7c7e666cba7e02e274e4fae42c56133f0deac
Jeff Mullins <jtmullins64@gmail.com>
2018-02-20 20:03:14 -0600

    chap 6 updates

--- Chapter 6/dictionaries.py
+++ Chapter 6/dictionaries.py
@@ -1,3 +1,28 @@
 alien_0 = {'color': 'green','points': 5}
-print(alien_0['color'])
-print(alien_0['points'])
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y-position'] = 25
+print(alien_0)
+
+alien_0['color'] = 'yellow'
+print(alien_0)
+
+alien_0 = {'x-position': 0, 'y-postion': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x-position']))
+#Move alien to the right
+#Determine how far to move based on speed
+if alien_0['speed'] =='slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+alien_0['x-position'] = alien_0['x-position'] + x_increment
+print("New x-position: " + str(alien_0['x-position']))
+
+fav_lang = {'jeff': 'python', 'jen': 'c++'}
+print("Jeff's favorite programming language is " + fav_lang['jeff'].title())
+
+person_details = {'first_name': 'frank', 'last_name': 'supancic', 'city': "kansas city", 'age': '31'}
+print(person_details['first_name'].title())
